c5a3c523e0b064aa2a61390a68449d4e8d7a0da8
Owen <ostevens@andrew.cmu.edu>
2018-09-04 18:07:15 -0400

    fixed plotly for scatterplot

---
+++ app.R
@@ -0,0 +1,64 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+library(plotly)
+library(reshape2)
+library(ggplot2)
+library(shiny)
+library(dplyr)
+cars <- mtcars
+cars$model <- rownames(mtcars)
+cars[c('drat','vs','am','gear','carb')] <-  NULL
+meltcars <- melt(cars, id = 'model')
+meltcars$model <- as.factor(meltcars$model)
+
+pdf(NULL)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("MTCars Data"),
+   
+   fluidRow(
+       # Sidebar with a slider input for number of bins 
+      sidebarLayout(
+          # Input x and y for scatterplot
+          sidebarPanel(
+            # select Y input
+             selectInput('y',
+                         "Y axis:",
+                         choices = colnames(cars),
+                         selected = "mpg"),
+             selectInput('x',
+                         'X axis:',
+                         choice = colnames(cars),
+                         selected = "wt")
+          ),
+                     
+          # Show a plot of the generated distribution
+          mainPanel(
+             plotOutput("scatterplot")
+          )
+       )
+   ),
+   fluidRow()
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$scatterplot <- renderPlot({
+      #generate scatterplot
+      ggplot(cars, aes_string(x = input$x, y = input$y))+geom_smooth()
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
